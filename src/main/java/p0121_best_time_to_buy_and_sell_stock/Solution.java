d5885e8ccf534e508d13fbc454136d09470e83d0
7blak <theblack2233@gmail.com>
2025-10-28 16:33:05 +0100

    Task 121 improved solution

--- src/main/java/p0121_best_time_to_buy_and_sell_stock/Solution.java
+++ src/main/java/p0121_best_time_to_buy_and_sell_stock/Solution.java
@@ -7,14 +7,15 @@ package p0121_best_time_to_buy_and_sell_stock;
  */
 public class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[j] > prices[i] && prices[j] - prices[i] > maxProfit) {
-                    maxProfit = prices[j] - prices[i];
-                }
+        int maxProfit = 0, candidateBuy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < candidateBuy) {
+                candidateBuy = prices[i];
+            }
+            else if (prices[i] - candidateBuy > maxProfit) {
+                maxProfit = prices[i] - candidateBuy;
             }
         }
-        return maxProfit;
+        return Math.max(maxProfit, 0);
     }
 }
