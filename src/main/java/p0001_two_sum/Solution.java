060b6977732864f01ee79253e7df1a0409e8b586
7blak <theblack2233@gmail.com>
2025-10-28 15:18:15 +0100

    Task 1 improved solution

--- src/main/java/p0001_two_sum/Solution.java
+++ src/main/java/p0001_two_sum/Solution.java
@@ -1,4 +1,8 @@
 package p0001_two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/two-sum/">1. Two Sum</a>
  * <br><br>
@@ -6,14 +10,14 @@ package p0001_two_sum;
  */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i, j;
-        for (i = 0; i < nums.length; i++) {
-            for (j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+        return new int[] {-1, -1};
     }
 }
